90f3613f1ce816391227a1fd318d0af3329eacc4
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-04-04 18:26:24 +0200

    Finished clock program

--- ClockProgram.js
+++ ClockProgram.js
@@ -3,14 +3,15 @@
 const changeToUsaTime = document.getElementById("US")
 const changeToEuTime = document.getElementById("EU")
 
-//Times
+//Deafult timer
 
-
-
-
-
+let defaultTimer;
 
 function usaTime(){
+    const timeUSA = new Date();
+    let hour = timeUSA.getHours();
+    const minutes = timeUSA.getMinutes().toString().padStart(2, 0);
+    const seconds = timeUSA.getSeconds().toString().padStart(2, 0);
     const usAmPm = hour >= 12 ? `PM` : "AM"
     if(hour % 12 === 0){
         hour = 12;
@@ -18,20 +19,38 @@ function usaTime(){
     else{
         hour = hour % 12;
     }
+    hour = hour.toString().padStart(2, 0);
     const timeDisplay = `${hour}:${minutes}:${seconds} ${usAmPm}`
     document.getElementById("clock").textContent = timeDisplay;
 }
 
 function euTime(){
-    const time = new Date();
-    let hour = time.getHours().toString().padStart(2, 0);
-    const minutes = time.getMinutes().toString().padStart(2, 0);
-    const seconds = time.getSeconds().toString().padStart(2, 0);
+    const timeEU = new Date();
+    const hour = timeEU.getHours().toString().padStart(2, 0);
+    const minutes = timeEU.getMinutes().toString().padStart(2, 0);
+    const seconds = timeEU.getSeconds().toString().padStart(2, 0);
     const timeDisplay = `${hour}:${minutes}:${seconds}`
     document.getElementById("clock").textContent = timeDisplay;
 }
 
-setInterval(euTime, 1000);
+euTime();
 
-// changeToEuTime.onclick = euTime;
-// changeToUsaTime.onclick = usaTime;
+defaultTimer = setInterval(euTime, 1000);
+
+
+changeToUsaTime.onclick = function() {
+    clearInterval(defaultTimer);
+    usaTime(); 
+    defaultTimer = setInterval(usaTime, 1000);
+};
+
+changeToEuTime.onclick = function() {
+    clearInterval(defaultTimer); 
+    euTime(); 
+    defaultTimer = setInterval(euTime, 1000); 
+};
+    
+
+    
+
+
